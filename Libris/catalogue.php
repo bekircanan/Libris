66d640ecd9e1781c746b6ffff5ac28bc647440d2
SERIANI selsebil <selsebil.seriani@gmail.com>
2024-11-21 18:35:34 +0100

    - finition de la page panier - debut de la page catalogue

--- Libris/catalogue.php
+++ Libris/catalogue.php
@@ -1,16 +1,78 @@
 <?php
     require_once 'header.php';
+    
+    try {
+        $conn = new PDO("mysql:host=localhost;dbname=libris", 'root', '');
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+    //:recherche
+
+        //requette sql pour la barre de recherche
+        $stmtRecherche = $conn->prepare("SELECT DISTINCT l.titre_livre, a.prenom_auteur, a.nom_auteur,  eb.prix
+                                FROM livre l JOIN ebook eb ON l.id_livre = eb.id_ebook
+                                            JOIN a_ecrit ae ON l.id_livre = ae.id_auteur
+                                            JOIN auteur a ON ae.id_auteur = a.id_auteur
+                                            JOIN livre_genre lg ON lg.id_livre = l.id_livre
+                                            JOIN genre g ON lg.id_genre = g.id_genre    
+                                            JOIN livre_edition le ON l.id_livre = le.id_livre
+                                            JOIN edition ed ON ed.id_edition = le.id_edition
+                                            JOIN livre_langue ll ON ll.id_livre = l.id_livre
+                                            JOIN langue lang ON lang.id_langue = ll.id_langue
+                                            JOIN livre_public lp ON lp.id_livre = l.id_livre
+                                            JOIN public_cible pc ON pc.id_public = lp.id_public
+                                WHERE :recherche LIKE pc.type_public
+                                OR :recherche LIKE g.nom_genre
+                                OR :recherche LIKE l.titre_livre
+                                OR :recherche LIKE l.type_litteraire
+                                OR :recherche LIKE lang.nom_langue
+                                OR :recherche LIKE ed.nom_edition
+                                OR :recherche LIKE a.nom_auteur"); 
+        
+        $stmtRecherche->execute([':recherche' => 'Roman']);
+        $infoLivres = $stmtRecherche->fetchAll();
+
+        $resultGenre = $conn->query("SELECT nom_genre FROM genre");
+
+        //if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        
+
 ?>
+
 <div id="page_catalogue">
-    <section id="filtre">
+    <section class="filters">
         <h1>Filtre</h1>
         <hr>
-        <h3>E-book</h3>
-        <h3>Genre de livres</h3>
-        <form><input type="checkbox" name="" value="" ></form> <!-- faire une requette pour afficher les genre -->
-        
-        <h3>Note</h3>
-    </section>
+        <section>
+            <h3>E-book</h3>
+            <input type="checkbox" id="ebook" name="ebook">
+            <label for="ebook">E-book</label>
+            <label for="price">Prix :</label>
+            <input type="range" id="price" min="0" max="50" value="25">
+        </section>
+        <section>
+            <h3>Genre de livres</h3>
+            <ul>
+                <?php
+                    foreach ($resultGenre as $genre){
+                        echo '<li><input type="checkbox" id="' . $genre['nom_genre'] . '"><label for="' . $genre['nom_genre'] . '">' . $genre['nom_genre'] . '</label></li>';
+                    }
+                ?>
+            </ul>
+        </section>
+        <section>
+            <h3>Note</h3>
+            <input type="checkbox" id="plus3etoiles" name="note">
+            <label for="plus3etoiles">Plus de 3 étoiles</label>
+            <input type="checkbox" id="moins3etoiles" name="note">
+            <label for="moins3etoiles">Moins de 3 étoiles</label>
+        </section>
+        <section>
+            <label for="parution">Date de parution :</label>
+            <input type="range" id="parution" min="0" max="50" value="10">
+        </section>
+</section>
     <section id="catalogue">
         <label>Trier par :</label><form></form>
     </section>
--- Libris/panier.php
+++ Libris/panier.php
@@ -3,7 +3,7 @@
 
     // faire une constante pour verif si le mec a payer pour garder la pop up afficher apres le rechargement de pages (si true on passe le display en flex, si false on passe le display en none)
     use PHPMailer\PHPMailer\PHPMailer;
-    require '..\vendor\autoload.php';
+    require '.\vendor\autoload.php';
     $errlog = '';
     function smtp($email, $subject, $body){
         $mail = new PHPMailer();
@@ -39,55 +39,56 @@
     } catch (PDOException $e) {
         die($e->getMessage());
     }
+//$_SESSION['id']
 
-    $stmt = $conn->prepare("SELECT DISTINCT l.titre_livre, a.nom_auteur, e.prix, achat.id_achat, e.lien_PDF
-                            FROM achat_ebook achat JOIN ebook e ON e.id_ebook = achat.id_ebook 
+    $stmtAfficheEbook = $conn->prepare("SELECT DISTINCT l.titre_livre, a.nom_auteur, e.prix, achat.id_achat, e.lien_PDF
+                                        FROM achat_ebook achat JOIN ebook e ON e.id_ebook = achat.id_ebook 
                                                 JOIN livre l ON e.id_livre = l.id_livre 
                                                 JOIN a_ecrit ae ON ae.id_livre = l.id_livre 
                                                 JOIN auteur a ON ae.id_auteur = a.id_auteur 
-                            WHERE id_util LIKE :idUtilisateur "); //AND regle = 0
+                                        WHERE id_util LIKE :idUtilisateur AND regle = 0");
         
-    $stmt->execute([':idUtilisateur' =>3]); //modifier avec l'id de l'utilisateur qui a ouvert la session
-    $achatEbook = $stmt->fetchAll();
+    $stmtAfficheEbook->execute([':idUtilisateur' => 2]);
+    $achatEbook = $stmtAfficheEbook->fetchAll();
     
     $prixTotal = 0;
     $listeLienEbook = '';
     foreach($achatEbook as $ebook){
         $prixTotal = $prixTotal + $ebook['prix'];
         $listeLienEbook = $listeLienEbook . ' ' . $ebook['titre_livre'] . ' : ' . $ebook['lien_PDF']; //"\n"
-       
     }
-    
+
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        if(isset($_POST['supprimer']) && !isset($_POST['payer'])){
-            $stmtDeleteEbook = $conn->prepare(
-                "DELETE FROM achat_ebook WHERE id_ebook = {$_POST['supprimer']}"
-            );
-            $stmtDeleteEbook->execute();
-
-        }else if(isset($_POST['payer']) && !isset($_POST['supprimer'])){
-            $stmt = $conn->prepare("SELECT email FROM utilisateur WHERE id_util = :idUtilisateur");
-            //$stmt2 = $conn2->prepare("UPDATE achat_ebook SET regle = 1, date_achat = TO_CHAR(SYSDATE, 'DD/MM/YYYY HH24:MI:SS') WHERE id_util = :idUtilisateur");
-            $stmt->execute([':idUtilisateur' =>3]); //modifier avec l'id de l'utilisateur qui a ouvert la session
-            $user = $stmt->fetch();
-
-            // si l'utilisateur existe, on crée un token et on l'envoie par mail
-            if($user){
-                $token = bin2hex(random_bytes(16));
-                $expire = date('Y-m-d H:i:s', time() + 60 * 15);
-                $stmt = $conn->prepare("INSERT INTO token (nom_token, expire, id_util) VALUES (:nomToken, :expire, :idUtilisateur)");
-                $stmt->execute([':expire' => $expire, ':nomToken' => $token, ':idUtilisateur' =>3]);//modifier avec l'id de l'utilisateur qui a ouvert la session
-                $errlog = smtp($user['email'], 'Merci pour votre achat sur Libris !', 'Merci pour cette transaction sur Libris.' . $listeLienEbook);
-            } else {
-                $errlog ='Aucun compte n\'est associé à cet email.';
+        if ($_POST['form']=== 'supprimer' || $_POST['form']=== 'payer'){
+            if(isset($_POST['supprimer']) && !isset($_POST['payer'])){
+                $stmtDeleteEbook = $conn->prepare("DELETE FROM achat_ebook WHERE id_ebook = :idEbook");
+                $stmtDeleteEbook->execute([':idEbook' => $_POST['supprimer']]);
+    
+            }else if(isset($_POST['payer']) && !isset($_POST['supprimer']) && $prixTotal !== 0){
+                $_SESSION['afficherPopUp'] = true;
+                $stmtRecupEmail = $conn->prepare("SELECT email FROM utilisateur WHERE id_util = :idUtilisateur");
+                $stmtRecupEmail->execute([':idUtilisateur' => 2]); 
+                $user = $stmtRecupEmail->fetch();
+    
+                $stmtModifDateAchat = $conn->prepare("UPDATE achat_ebook SET regle = 1, date_achat = NOW() WHERE id_util = :idUtilisateur");
+                $stmtModifDateAchat->execute([':idUtilisateur' => 2]);
+    
+                // si l'utilisateur existe, on crée un token et on l'envoie par mail
+                if($user){
+                    $token = bin2hex(random_bytes(16));
+                    $expire = date('Y-m-d H:i:s', time() + 60 * 15);
+                    $stmt = $conn->prepare("INSERT INTO token (nom_token, expire, id_util) VALUES (:nomToken, :expire, :idUtilisateur)");
+                    $stmt->execute([':expire' => $expire, ':nomToken' => $token, ':idUtilisateur' => 2]);
+                    $errlog = smtp($user['email'], 'Merci pour votre achat sur Libris !', 'Merci pour cette transaction sur Libris. Voici le lien de téléchargement de chaques livres : '. "\n" . $listeLienEbook);
+                } else {
+                    $errlog ='Aucun compte n\'est associé à cet email.';
+                }
             }
         }
         
+        
     }
     ?>
-
-
-    
 
     <h1 id="mon_panier">Mon panier</h1>
     <div class="prix_section">
@@ -96,61 +97,77 @@
             
             <div id="prix">
                 <p>Prix Total</p>
-                <p><?php echo $prixTotal. '€'?></p>
+                <p><?php echo ($prixTotal === 0 ? "-" : $prixTotal . "€");?></p>
             </div>
             <hr> 
             <div id="payer">
                 <a href=".\catalogue.php"> < Retour au catalogue</a> 
                 <form method="POST">
+                    <input type="hidden" name="form" value="payer">
                     <button name="payer" class="bouton_payer">Payer</button>
                 </form>
 
             </div>
 
-            
-                
-            
         </section>
+
         <div class="mask"></div>
+
         <div class="modal">
             <p>Un mail a été envoyé</p>
             <button href="panier.php"class="close">X</button>
         </div>
+        
+        <?php
+            if ($prixTotal === 0){
+                echo '<h1>Votre panier est vide</h1>';
+                echo '<a href=".\catalogue.php">Choisir des ebooks</a>';
+            }else{
+                foreach($achatEbook as $ebook){
+                    echo '<div class="ebook_acheter">'; 
+                    echo '<p>'. 'imageaaaaaaaa' . '</p>';
+                    echo '<div class="info_ebook_acheter">';
+                    echo '<p>' . $ebook['titre_livre'] . '</p>';
+                    echo '<p>' . $ebook['nom_auteur'] . '</p>';
+                    echo '<p>' . $ebook['prix'] . '€</p>';
+                    echo '</div>'; 
+                    echo '<form method="POST"><input type="hidden" name="form" value="supprimer"><button name="supprimer" value =" '. $ebook['id_achat'] . '">Supprimer <img id="poubelle" src="./img/icon_poubelle.png" alt="Icône de poubelle" </button> </form>';
+                    echo '</div>'; 
+                } 
+            }
+        ?>
+    </div>
 
-        <script>
-            let mask = document.querySelector(".mask")
-            let modal = document.querySelector(".modal")
+    <!-- script qui permet d'afficher une pop up confirmant l'envoie du mail -->
+    <script>
+            
+            let mask = document.querySelector(".mask");
+            let modal = document.querySelector(".modal");
 
-            let test = document.querySelector(".close");
-            test.addEventListener('click',closePopUp);
-            let test2 = document.querySelector(".bouton_payer");
-            test2.addEventListener('click',openPopUp);
-
+            let boutonFermer = document.querySelector(".close");
+            boutonFermer.addEventListener('click',closePopUp);
+            
+            let boutonOuvrir = document.querySelector(".bouton_payer");
+            boutonOuvrir.addEventListener('click',openPopUp);
+            if ($_SESSION['afficherPopUp'] == true){
+                openPopUp();
+            }
             function openPopUp() {
-                modal.classList.toggle("modal_change");
-                mask.classList.toggle("mask_change"); // Show the background overlay
+                if ($prixTotal != 0){
+                    modal.classList.toggle("modal_change");
+                    mask.classList.toggle("mask_change");
+                }
             }
 
             function closePopUp() {
-                modal.classList.toggle("modal");
-                mask.classList.toggle("mask"); // Hide the background overlay
+                if ($prixTotal != 0){
+                    modal.classList.toggle("modal");
+                    mask.classList.toggle("mask");
+                    $_SESSION['afficherPopUp'] = false;
+                }
             }
+            
         </script>
-        
-        <?php
-            foreach($achatEbook as $ebook){
-                echo '<div class="ebook_acheter">'; 
-                echo '<p>'. 'imageaaaaaaaa' . '</p>';
-                echo '<div class="info_ebook_acheter">';
-                echo '<p>' . $ebook['titre_livre'] . '</p>';
-                echo '<p>' . $ebook['nom_auteur'] . '</p>';
-                echo '<p>' . $ebook['prix'] . '€</p>';
-                echo '</div>'; 
-                echo '<form method="POST"><button name="supprimer" value =" '. $ebook['id_achat'] . '">Supprimer <img id="poubelle" src="./img/icon_poubelle.png" alt="Icône de poubelle" </button> </form>';
-                echo '</div>'; 
-            } 
-        ?>
-    </div>
     
 <?php
     require_once 'footer.php';
