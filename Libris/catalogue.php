58e11e2a99093262e9a1e92e9990786df9cb398d
SERIANI selsebil <selsebil.seriani@gmail.com>
2024-11-22 20:59:33 +0100

    -ajout de l'affichage des livre dans la page catalogue

--- Libris/catalogue.php
+++ Libris/catalogue.php
@@ -7,21 +7,20 @@
     } catch (PDOException $e) {
         die($e->getMessage());
     }
-    //:recherche
-
+    
         //requette sql pour la barre de recherche
-        $stmtRecherche = $conn->prepare("SELECT DISTINCT l.titre_livre, a.prenom_auteur, a.nom_auteur,  eb.prix
-                                FROM livre l JOIN ebook eb ON l.id_livre = eb.id_ebook
-                                            JOIN a_ecrit ae ON l.id_livre = ae.id_auteur
-                                            JOIN auteur a ON ae.id_auteur = a.id_auteur
-                                            JOIN livre_genre lg ON lg.id_livre = l.id_livre
-                                            JOIN genre g ON lg.id_genre = g.id_genre    
-                                            JOIN livre_edition le ON l.id_livre = le.id_livre
-                                            JOIN edition ed ON ed.id_edition = le.id_edition
-                                            JOIN livre_langue ll ON ll.id_livre = l.id_livre
-                                            JOIN langue lang ON lang.id_langue = ll.id_langue
-                                            JOIN livre_public lp ON lp.id_livre = l.id_livre
-                                            JOIN public_cible pc ON pc.id_public = lp.id_public
+        $stmtRecherche = $conn->prepare("SELECT DISTINCT l.id_livre, l.titre_livre, a.prenom_auteur, a.nom_auteur, eb.prix
+                                FROM livre l LEFT OUTER JOIN ebook eb ON l.id_livre = eb.id_ebook
+                                            LEFT OUTER JOIN a_ecrit ae ON l.id_livre = ae.id_auteur
+                                            LEFT OUTER JOIN auteur a ON ae.id_auteur = a.id_auteur
+                                            LEFT OUTER JOIN livre_genre lg ON lg.id_livre = l.id_livre
+                                            LEFT OUTER JOIN genre g ON lg.id_genre = g.id_genre    
+                                            LEFT OUTER JOIN livre_edition le ON l.id_livre = le.id_livre
+                                            LEFT OUTER JOIN edition ed ON ed.id_edition = le.id_edition
+                                            LEFT OUTER JOIN livre_langue ll ON ll.id_livre = l.id_livre
+                                            LEFT OUTER JOIN langue lang ON lang.id_langue = ll.id_langue
+                                            LEFT OUTER JOIN livre_public lp ON lp.id_livre = l.id_livre
+                                            LEFT OUTER JOIN public_cible pc ON pc.id_public = lp.id_public
                                 WHERE :recherche LIKE pc.type_public
                                 OR :recherche LIKE g.nom_genre
                                 OR :recherche LIKE l.titre_livre
@@ -30,9 +29,11 @@
                                 OR :recherche LIKE ed.nom_edition
                                 OR :recherche LIKE a.nom_auteur
                                 OR :recherche LIKE l.cote_livre"); 
-        
-        $stmtRecherche->execute([':recherche' => 'Roman']);
-        $infoLivres = $stmtRecherche->fetchAll();
+        //ajouter une requette qui recup les auteur pour tel livre car s'il y en a plusieur on les recup pas tous.
+        //faire la meme pour compter les avis et rajouter le nb d'étoile.
+        //recherche dois etre recup depuis l'url tout comme amadis a fait dans la page info_livre
+        $stmtRecherche->execute([':recherche' => 'roman']);
+        $livreRecherche = $stmtRecherche->fetchAll();
 
         $resultGenre = $conn->query("SELECT nom_genre FROM genre");
 
@@ -73,9 +74,28 @@
             <label for="parution">Date de parution :</label>
             <input type="range" id="parution" min="0" max="50" value="10">
         </section>
-</section>
+    </section>
     <section id="catalogue">
-        <label>Trier par :</label><form></form>
+        <label for="tri-select">Trier par :</label>
+        <select name="tri" id="tri-select">
+            <option value="a-z">A-Z</option>
+            <option value="z-a">Z-A</option>
+        </select>
+        <div class="liste-ebook">
+            <?php 
+            //faire en sorte que lorsque l'on click sur un livre on ça nous redirige sur la page info_livre avec l'id du livre en tete
+                foreach($livreRecherche as $livre){
+                    //ajouter dans l'url l'id du livre click voire blog
+                    echo '<a href="../info_livre.php"><div class="book-item">';
+                    echo '<h3>' . $livre['titre_livre'] . '</h3>';
+                    echo '<p>' . $livre['prenom_auteur'] . ' ' . $livre['nom_auteur'] . '</p>';
+                    if($livre['prix'] != null){
+                        echo '<p><span class="price">' . $livre['prix'] . '</span></p>';
+                    }
+                    echo '</div></a>';
+                }
+            ?>
+        </div>
     </section>
 </div>
 <?php
