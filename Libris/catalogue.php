31bab5693c03aa2a0678b331f20d664a1524f543
SERIANI selsebil <selsebil.seriani@gmail.com>
2024-11-26 10:05:30 +0100

    - ajout de la page catalogue et son css

--- Libris/catalogue.php
+++ Libris/catalogue.php
@@ -1,43 +1,66 @@
 <?php
     require_once 'header.php';
-    
-    try {
-        $conn = new PDO("mysql:host=localhost;dbname=libris", 'root', '');
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        die($e->getMessage());
-    }
-    
-        //requette sql pour la barre de recherche
-        $stmtRecherche = $conn->prepare("SELECT DISTINCT l.id_livre, l.titre_livre, a.prenom_auteur, a.nom_auteur, eb.prix
-                                FROM livre l LEFT OUTER JOIN ebook eb ON l.id_livre = eb.id_ebook
-                                            LEFT OUTER JOIN a_ecrit ae ON l.id_livre = ae.id_auteur
-                                            LEFT OUTER JOIN auteur a ON ae.id_auteur = a.id_auteur
-                                            LEFT OUTER JOIN livre_genre lg ON lg.id_livre = l.id_livre
-                                            LEFT OUTER JOIN genre g ON lg.id_genre = g.id_genre    
-                                            LEFT OUTER JOIN livre_edition le ON l.id_livre = le.id_livre
-                                            LEFT OUTER JOIN edition ed ON ed.id_edition = le.id_edition
-                                            LEFT OUTER JOIN livre_langue ll ON ll.id_livre = l.id_livre
-                                            LEFT OUTER JOIN langue lang ON lang.id_langue = ll.id_langue
-                                            LEFT OUTER JOIN livre_public lp ON lp.id_livre = l.id_livre
-                                            LEFT OUTER JOIN public_cible pc ON pc.id_public = lp.id_public
-                                WHERE :recherche LIKE pc.type_public
-                                OR :recherche LIKE g.nom_genre
-                                OR :recherche LIKE l.titre_livre
-                                OR :recherche LIKE l.type_litteraire
-                                OR :recherche LIKE lang.nom_langue
-                                OR :recherche LIKE ed.nom_edition
-                                OR :recherche LIKE a.nom_auteur
-                                OR :recherche LIKE l.cote_livre"); 
-        //ajouter une requette qui recup les auteur pour tel livre car s'il y en a plusieur on les recup pas tous.
-        //faire la meme pour compter les avis et rajouter le nb d'étoile.
-        //recherche dois etre recup depuis l'url tout comme amadis a fait dans la page info_livre
-        $stmtRecherche->execute([':recherche' => 'roman']);
-        $livreRecherche = $stmtRecherche->fetchAll();
 
-        $resultGenre = $conn->query("SELECT nom_genre FROM genre");
+    // if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    //     $infoRecherche = $_GET['recherche'];
+    // }    
+    
+    // if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    //     if ($_POST['form']=== 'trie'){
+    //         $valueTri = $_POST["tri"];
+    //         if ($valueTri === "z-a") {
+    //             $_SESSION['trie'] = 'DESC';
+                
+    //         } elseif ($valueTri === "a-z"){
+    //             echo 'test';
+    //             unset($_SESSION['trie']);
+                
+    //         }
+    //     }
+        
+    // }
 
-        //if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $stmtRecherche = $conn->prepare("SELECT DISTINCT l.id_livre, l.titre_livre, eb.prix
+                                    FROM livre l LEFT OUTER JOIN ebook eb ON l.id_livre = eb.id_livre
+                                        LEFT OUTER JOIN a_ecrit ae ON l.id_livre = ae.id_livre
+                                        LEFT OUTER JOIN auteur a ON ae.id_auteur = a.id_auteur
+                                        LEFT OUTER JOIN livre_genre lg ON lg.id_livre = l.id_livre
+                                        LEFT OUTER JOIN genre g ON lg.id_genre = g.id_genre    
+                                        LEFT OUTER JOIN livre_edition le ON l.id_livre = le.id_livre
+                                        LEFT OUTER JOIN edition ed ON ed.id_edition = le.id_edition
+                                        LEFT OUTER JOIN livre_langue ll ON ll.id_livre = l.id_livre
+                                        LEFT OUTER JOIN langue lang ON lang.id_langue = ll.id_langue
+                                        LEFT OUTER JOIN livre_public lp ON lp.id_livre = l.id_livre
+                                        LEFT OUTER JOIN public_cible pc ON pc.id_public = lp.id_public
+                                    WHERE :recherche LIKE pc.type_public
+                                        OR :recherche LIKE g.nom_genre
+                                        OR :recherche LIKE l.titre_livre
+                                        OR :recherche LIKE l.type_litteraire
+                                        OR :recherche LIKE lang.nom_langue
+                                        OR :recherche LIKE ed.nom_edition
+                                        OR :recherche LIKE a.nom_auteur
+                                        OR :recherche LIKE l.cote_livre
+                                    ORDER BY l.titre_livre "); 
+
+    $stmtRecherche->execute([':recherche' => 'roman']);//$infoRecherche]);
+    $livreRecherche = $stmtRecherche->fetchAll();
+
+    $stmtNbAvis = $conn->prepare("SELECT COUNT(id_avis) as avis
+                                    FROM avis
+                                    WHERE id_livre = :id_livre ");
+                                                    
+    $stmtAuteur = $conn->prepare("SELECT DISTINCT a.nom_auteur, a.prenom_auteur 
+                                    FROM auteur a LEFT OUTER JOIN a_ecrit ae ON a.id_auteur = ae.id_auteur
+                                    WHERE id_livre = :id_livre ");
+    
+    // $index = 0;
+    // foreach($livreRecherche as $livre){
+    //     $listeIdLivre[]
+    //     $index = $index + 1
+    // }
+
+    $resultGenre = $conn->query("SELECT nom_genre FROM genre");
+
         
 
 ?>
@@ -76,23 +99,40 @@
         </section>
     </section>
     <section id="catalogue">
-        <label for="tri-select">Trier par :</label>
-        <select name="tri" id="tri-select">
-            <option value="a-z">A-Z</option>
-            <option value="z-a">Z-A</option>
-        </select>
+        <?php echo 'la' . $_SESSION['trie']; ?>
+        <form method="POST" action="">
+            <input type="hidden" name="form" value="trie">
+            <label for="tri-select">Trier par :</label>
+            <select name="tri" id="tri-select" onchange="this.form.submit()">
+                <option value="a-z">A-Z</option>
+                <option value="z-a">Z-A</option>
+            </select>
+        </form>
         <div class="liste-ebook">
-            <?php 
-            //faire en sorte que lorsque l'on click sur un livre on ça nous redirige sur la page info_livre avec l'id du livre en tete
+            <?php
                 foreach($livreRecherche as $livre){
-                    //ajouter dans l'url l'id du livre click voire blog
-                    echo '<a href="../info_livre.php"><div class="book-item">';
+                    $lesAuteurs = '';
+                    echo '<div class="book-item"><a href="./info_livre.php?id_livre=' . $livre['id_livre']. '">';
                     echo '<h3>' . $livre['titre_livre'] . '</h3>';
-                    echo '<p>' . $livre['prenom_auteur'] . ' ' . $livre['nom_auteur'] . '</p>';
-                    if($livre['prix'] != null){
-                        echo '<p><span class="price">' . $livre['prix'] . '</span></p>';
+                    
+                    $stmtAuteur->execute([':id_livre' => $livre['id_livre']]);
+                    $Auteurs = $stmtAuteur->fetchAll();
+                    foreach($Auteurs as $aut){
+                        $lesAuteurs = $lesAuteurs . $aut['prenom_auteur'] . " " . $aut['nom_auteur'];
                     }
-                    echo '</div></a>';
+                    echo '<p>' . $lesAuteurs     . '</p>';
+
+                    if($livre['prix'] != null){
+                        echo '<p><span class="price"> Prix : ' . $livre['prix'] . '</span></p>';
+                    }
+
+                    $stmtNbAvis->execute([':id_livre' => $livre['id_livre']]);
+                    $NbAvis = $stmtNbAvis->fetchAll();
+                    foreach($NbAvis as $avis){
+                        echo '<p> avis : ' . $avis['avis'] . '</p>';
+                    }
+                    
+                    echo '</a></div>';
                 }
             ?>
         </div>
